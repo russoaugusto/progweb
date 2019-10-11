f7d187fb080357f5f4081b302028e6bbfdbf1fec
Augusto <119215293@animaedu.intranet>
2019-10-11 20:04:52 -0300

    classe Pessoa criada e printando o objeto no index

---
+++ Pessoa.php
@@ -0,0 +1,45 @@
+<?php
+
+class Pessoa {
+	private $altura;
+	private $idade;
+	private $peso;
+
+	public function getPeso(){
+		return $this->peso;
+	}
+
+	public function getIdade(){
+		return $this->idade;
+	}
+
+	public function getAltura(){
+		return $this->altura;
+	}
+
+	public function setAltura(){
+		$this->altura = $altura;
+	}
+
+	public function setIdade(){
+		$this->idade = $idade;
+	}
+
+	public function setPeso(){
+		$this->peso = $peso;
+	}
+
+	public function __construct($peso, $idade, $altura){
+		$this->peso = $peso;
+		$this->idade = $idade;
+		$this->altura = $altura;
+	}
+
+	public function __toString(){
+		$string = 
+			"Peso ".$this->peso."<br>".
+			"Idade ".$this->idade."<br>".
+			"Altura ".$this->altura."<br>";
+		return $string;
+	}
+}
